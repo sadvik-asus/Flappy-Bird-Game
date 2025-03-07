612371afeaf26ceb25de3011c8a4f29b169b0a91
v sadvik kumar <142149067+sadvik-asus@users.noreply.github.com>
2025-03-07 22:01:55 +0530

    Update pipe.py

--- pipe.py
+++ pipe.py
@@ -6,8 +6,8 @@ from random import randint
 class Pipe:
     def __init__(self,scale_factor,move_speed):
         # loading the pipe images 
-        self.img_up=pg.transform.scale_by(pg.image.load("assets/pipeup.png").convert_alpha(),scale_factor)
-        self.img_down=pg.transform.scale_by(pg.image.load("assets/pipedown.png").convert_alpha(),scale_factor)
+        self.img_up=pg.transform.scale_by(pg.image.load("Assets/pipeup.png").convert_alpha(),scale_factor)
+        self.img_down=pg.transform.scale_by(pg.image.load("Assets/pipedown.png").convert_alpha(),scale_factor)
         self.rect_up=self.img_up.get_rect()
         self.rect_down=self.img_down.get_rect()
         self.pipe_distance=150
